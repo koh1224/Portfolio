bf6b517833cb99d1cb3e66411aeb71981386c149
EC2 Default User <ec2-user@ip-172-31-26-75.ap-northeast-1.compute.internal>
2021-11-20 10:26:06 +0000

    add signin,up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 10 }, uniqueness: true
+
+  attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
   root to: 'homes#top'
   get "/home/about" => "homes#about"
 
+  resources :users
   resources :ideas
 
 
