cc7cb4cb268fdad4ac662b0ed85cae35ba74e1f5
EC2 Default User <ec2-user@ip-172-31-26-75.ap-northeast-1.compute.internal>
2021-11-26 18:12:45 +0000

    add user_info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @ideas = Idea.where(user_id: @user.id)
+    @idea = Idea.new
+    @ideafind = Idea.find(params[:id])
   end
 
   def edit
